3afde3f709fbb2e6485dc22a020fecb7ef34becd
maonadim <53486439+maonadim@users.noreply.github.com>
2022-05-22 00:29:38 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+
+
+
+st.title("Dashboards Finance")
+st.header("oracle capital Dashboard")
+st.write("cette application nous aide à analyser les marchés")
+
+tickers = ('TSLA','AAPL','MSFT','FB')
+dropdown = st.multiselect('Pick your assets',
+                          tickers)
+start = st.date_input('Start', value=pd.to_datetime('2021-01-01'))
+end = st.date_input('Start', value=pd.to_datetime('today'))
+
+if len(dropdown) > 0:
+    df = yf.download(dropdown,start,end)['Close']
+    st.line_chart(df)
